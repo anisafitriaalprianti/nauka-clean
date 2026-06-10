f9b7db5f6b6aae9419a7c7d07a9647109908fffe
anisafitriaalprianti <annisafitriaalprianti@gmail.com>
2026-06-10 13:39:11 +0700

    Create undangan/page.tsx

---
+++ app/undangan/page.tsx
@@ -0,0 +1,13 @@
+export default function Undangan() {
+  return (
+    <main style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "serif"
+    }}>
+      <h1>UNDANGAN NAUKA</h1>
+    </main>
+  )
+}
