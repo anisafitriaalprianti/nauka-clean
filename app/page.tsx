5464432146826e650bc703d452a322663f8cedd1
anisafitriaalprianti <annisafitriaalprianti@gmail.com>
2026-06-11 11:05:26 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div style={{ padding: 50, fontSize: 30, color: 'red' }}>
-      NAUKA FORCE TEST 999
+    <div style={{ padding: 50 }}>
+      NAUKA REBUILD FIX
     </div>
   );
 }
