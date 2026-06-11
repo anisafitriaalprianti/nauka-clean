d42c4deefa4a271cb8d72742fb35dd8d345ac542
anisafitriaalprianti <annisafitriaalprianti@gmail.com>
2026-06-11 11:28:36 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div
-      style={{
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <img
+    <div>
+      <Image
         src="/file_0000000077687209b2adc5b443052e18.png"
-        alt="NAUKA Logo"
-        style={{
-          width: 160,
-          height: 160,
-          objectFit: 'contain',
-        }}
+        width={160}
+        height={160}
+        alt="logo"
       />
-
-      <h1 style={{ margin: 0 }}>
-        NAUKA
-      </h1>
-
-      <p style={{ margin: 0 }}>
-        “Sebuah ruang kecil untuk mengantar momen”
-      </p>
     </div>
   );
 }
